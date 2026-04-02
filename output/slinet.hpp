21e6d911f0a23c6e4a0eaf2e9cc72979dec059fc
bbq5783 <15007604154@163.com>
2026-04-03 01:30:05 +0800

    重新上传 output 文件夹 2026-04-03 01:30:05

--- output/slinet.hpp
+++ output/slinet.hpp
@@ -5,7 +5,7 @@
 
 namespace cs2_dumper {
     namespace slient_offsets {
-        constexpr std::ptrdiff_t m_slientHookAddress = 0xC66271;
-        constexpr std::ptrdiff_t m_slientHookReturnAddress = 0xC6629A;
+        constexpr std::ptrdiff_t m_slientHookAddress = 0xC661D9;
+        constexpr std::ptrdiff_t m_slientHookReturnAddress = 0xC661E5;
     }
 }

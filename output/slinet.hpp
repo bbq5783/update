8f8af46f6683f9e015ef17754906790994c253f4
bbq5783 <15007604154@163.com>
2026-09-09 16:47:18 +0800

    Update slinet.hpp

--- output/slinet.hpp
+++ output/slinet.hpp
@@ -5,7 +5,7 @@
 
 namespace cs2_dumper {
     namespace slient_offsets {
-        constexpr std::ptrdiff_t m_slientHookAddress = 0xCA3B89;
-        constexpr std::ptrdiff_t m_slientHookReturnAddress = 0xCA3B95;
+        constexpr std::ptrdiff_t m_slientHookAddress = 0xCA3C49;
+        constexpr std::ptrdiff_t m_slientHookReturnAddress = 0xCA3C55;
     }
 }

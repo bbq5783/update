cbdddedee40b9daaa96d353df3fe40d5f453b7d4
bbq5783 <15007604154@163.com>
2026-09-11 11:56:35 +0800

    Add files via upload

--- output/slinet.hpp
+++ output/slinet.hpp
@@ -5,7 +5,7 @@
 
 namespace cs2_dumper {
     namespace slient_offsets {
-        constexpr std::ptrdiff_t m_slientHookAddress = 0xCA3C49;
-        constexpr std::ptrdiff_t m_slientHookReturnAddress = 0xCA3C55;
+        constexpr std::ptrdiff_t m_slientHookAddress = 0xCA8929;
+        constexpr std::ptrdiff_t m_slientHookReturnAddress = 0xCA8935;
     }
 }
